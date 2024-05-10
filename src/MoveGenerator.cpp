ed199e3c30454b5cef8bde79f5b14cb740d14fd6
giorgio <giorgio.lovato99@gmail.com>
2024-05-10 23:22:38 +0200

    pawn move generation now uses bitboards and precomputed captures. big speed improvement

--- src/MoveGenerator.cpp
+++ src/MoveGenerator.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 #include <cstdint>
 #include <iostream>
-// Testing of specific position: position startpos moves b1a3 a7a6
+// Testing of specific position: position startpos moves d5d6 h3g2 a2a3 
 //  These are all functionalities connected to move generation
 
 uint_fast64_t knightMoves[64];
@@ -27,8 +27,9 @@ void GetPseudoLegalPawnMoves(const Engine &engine, std::vector<Move> &pseudoLega
     auto color = engine.board.whiteToMove;
     auto colorIndex = color ? 0 : 6;
     auto colorDirection = color ? -8 : 8;
-    auto startRank = color ? 6 : 1;
-    auto promotionRank = color ? 0 : 7;
+    auto startRank = color ? 1 : 6;
+    auto promotionRank = color ? 7 : 0;
+    uint_fast64_t combinedColors = ~engine.board.colorBoards[color] & ~engine.board.colorBoards[!color];
 
     // Single Pawn pushes
     uint_fast64_t pushes = engine.board.pieceBoards[colorIndex]; // Get pawn bitboard of mover color
@@ -40,14 +41,15 @@ void GetPseudoLegalPawnMoves(const Engine &engine, std::vector<Move> &pseudoLega
     { // if black, push down
         pushes = pushes << 8;
     }
-    pushes &= (~engine.board.colorBoards[color] & ~engine.board.colorBoards[!color]); // remove pawns which landed on occupied squares
+    pushes &= combinedColors; // remove pawns which landed on occupied squares
     auto to = BitScanForwards(pushes)-1;
     while (to>=0)
     {
         Move move(to-colorDirection,to);
         if(CheckBit(rankMasks[promotionRank],to)){
-            for(auto i=1;i<5;i++){
+            for(auto i=2;i<6;i++){
                 move.convertTo = (PieceType)i;
+                move.promotion = true;
                 pseudoLegalMoves.push_back(move);
             }
         }
@@ -58,173 +60,51 @@ void GetPseudoLegalPawnMoves(const Engine &engine, std::vector<Move> &pseudoLega
         to = BitScanForwards(pushes)-1;
     }
     
-    // for (auto i = 0; i < 8; i++)
-    // {
-    //     auto j = startRank + colorDirection; // we only care for pawn moves between start rank and one short of promotion rank
-    //     for (auto k = 0; k < 5; k++)
-    //     {
-    //         if (CheckBit(pushes, i, j))
-    //         {
-    //             pseudoLegalMoves.push_back(Move(i, j - colorDirection, i, j)); // Push back all the remaining valid moves
-    //         }
-    //         j += colorDirection;
-    //     }
-    // }
-
-    // // Double pawn pushes - push all remaining pawns one more time
-    // uint_fast64_t doublePushes;
-    // if (color)
-    // { // if white push up
-    //     doublePushes = pushes >> 8;
-    // }
-    // else
-    // { // if black push down
-    //     doublePushes = pushes << 8;
-    // }
-    // doublePushes &= (~engine.board.colorBoards[color] & ~engine.board.colorBoards[!color]); // remove pawns which landed on occupied squares
-
-    // auto fourthRank = color ? 4 : 3; // valid double push target squares
-    // for (auto i = 0; i < 8; i++)
-    // {
-    //     if (CheckBit(doublePushes, i, fourthRank))
-    //     {
-    //         pseudoLegalMoves.push_back(Move(i, fourthRank - 2 * colorDirection, i, fourthRank)); // only push back moves which landed on valid double push squares
-    //     }
-    // }
+    // Double pawn pushes - push all pawns twice (check for blockers inbetween)
+    pushes = engine.board.pieceBoards[colorIndex] & rankMasks[startRank];
+    if (color)
+    { // if white push up
+        pushes = ((pushes >> 8)&combinedColors)>>8;
+    }
+    else
+    { // if black push down
+        pushes = ((pushes << 8)&combinedColors)<<8;
+    }
+    pushes &= combinedColors; // remove pawns which landed on occupied squares
+    to = BitScanForwards(pushes)-1;
+    while (to>=0)
+    {
+        pseudoLegalMoves.push_back(Move(to-colorDirection*2,to));
+        UnsetBit(pushes,to);
+        to = BitScanForwards(pushes)-1;
+    }
 
     // // Pawn Captures
-    // uint_fast64_t captures;
-    // if (color) // Shift pawn board diagonal up or down depending on color
-    // {
-    //     captures = engine.board.pieceBoards[colorIndex] >> 7 | engine.board.pieceBoards[colorIndex] >> 9;
-    // }
-    // else
-    // {
-    //     captures = engine.board.pieceBoards[colorIndex] << 7 | engine.board.pieceBoards[colorIndex] << 9;
-    // }
-    // captures = captures & (engine.board.colorBoards[color] | engine.board.ghostBoard); // check for capturable pieces there
-    // for (auto i = 1; i < 7; i++)                                                       // pawns at the edge need to be handled seperately to prevent telepawns
-    // {
-    //     auto j = startRank + colorDirection; // Again only consider captures between start rank and one short of promotion
-    //     for (auto k = 0; k < 5; k++)
-    //     {
-    //         if (CheckBit(captures, i, j))
-    //         {
-    //             if (CheckBit(engine.board.pieceBoards[colorIndex], i - 1, j - colorDirection)) // Captures are diagonal moves
-    //             {
-    //                 pseudoLegalMoves.push_back(Move(i - 1, j - colorDirection, i, j));
-    //             }
-    //             if (CheckBit(engine.board.pieceBoards[colorIndex], i + 1, j - colorDirection)) // Because two pawns might capture on the same square, we need to check from where the captures came
-    //             {
-    //                 pseudoLegalMoves.push_back(Move(i + 1, j - colorDirection, i, j));
-    //             }
-    //         }
-    //         j += colorDirection;
-    //     }
-    // }
-    // auto j = startRank + colorDirection; // Treatment of the border pawns
-    // for (auto k = 0; k < 5; k++)
-    // {
-    //     if (CheckBit(captures, 0, j))
-    //     {
-    //         if (CheckBit(engine.board.pieceBoards[colorIndex], 1, j - colorDirection))
-    //         {
-    //             pseudoLegalMoves.push_back(Move(1, j - colorDirection, 0, j));
-    //         }
-    //     }
-    //     j += colorDirection;
-    // }
-    // j = startRank + colorDirection;
-    // for (auto k = 0; k < 5; k++)
-    // {
-    //     if (CheckBit(captures, 7, j))
-    //     {
-    //         if (CheckBit(engine.board.pieceBoards[colorIndex], 6, j - colorDirection))
-    //         {
-    //             pseudoLegalMoves.push_back(Move(6, j - colorDirection, 7, j));
-    //         }
-    //     }
-    //     j += colorDirection;
-    // }
-
-    // // Promotion - if a pawn move ends on the promotion rank, we need to assign convert to to the moves
-    // // First nonborder pawns
-    // for (auto i = 1; i < 7; i++)
-    // {
-    //     if (CheckBit(pushes, i, promotionRank))
-    //     {
-    //         Move promotion = Move(i, promotionRank - colorDirection, i, promotionRank);
-    //         for (auto pieceType = 2; pieceType < 6; pieceType++)
-    //         {
-    //             promotion.convertTo = (PieceType)pieceType;
-    //             promotion.promotion = true;
-    //             pseudoLegalMoves.push_back(promotion);
-    //         }
-    //     }
-    //     if (CheckBit(captures, i, promotionRank))
-    //     {
-    //         if (CheckBit(engine.board.pieceBoards[colorIndex], i - 1, promotionRank - colorDirection)) // Captures are diagonal moves
-    //         {
-    //             Move promotion = Move(i - 1, promotionRank - colorDirection, i, promotionRank);
-    //             for (auto pieceType = 2; pieceType < 6; pieceType++)
-    //             {
-    //                 promotion.convertTo = (PieceType)pieceType;
-    //                 promotion.promotion = true;
-    //                 pseudoLegalMoves.push_back(promotion);
-    //             }
-    //         }
-    //         if (CheckBit(engine.board.pieceBoards[colorIndex], i + 1, promotionRank - colorDirection)) // Because two pawns might capture on the same square, we need to check from where the captures came
-    //         {
-    //             Move promotion = Move(i + 1, j - colorDirection, i, promotionRank);
-    //             for (auto pieceType = 2; pieceType < 6; pieceType++)
-    //             {
-    //                 promotion.convertTo = (PieceType)pieceType;
-    //                 promotion.promotion = true;
-    //                 pseudoLegalMoves.push_back(promotion);
-    //             }
-    //         }
-    //     }
-    // }
-    // // Now problematic border pawns
-    // for (auto i = 0; i < 8; i += 7)
-    // {
-    //     if (CheckBit(pushes, i, promotionRank))
-    //     {
-    //         Move promotion = Move(i, promotionRank - colorDirection, i, promotionRank);
-    //         for (auto pieceType = 2; pieceType < 6; pieceType++)
-    //         {
-    //             promotion.convertTo = (PieceType)pieceType;
-    //             promotion.promotion = true;
-    //             pseudoLegalMoves.push_back(promotion);
-    //         }
-    //     }
-    // }
-    // if (CheckBit(captures, 0, promotionRank))
-    // {
-    //     if (CheckBit(engine.board.pieceBoards[colorIndex], 0 + 1, promotionRank - colorDirection)) // Because two pawns might capture on the same square, we need to check from where the captures came
-    //     {
-    //         Move promotion = Move(0 + 1, promotionRank - colorDirection, 0, promotionRank);
-    //         for (auto pieceType = 2; pieceType < 6; pieceType++)
-    //         {
-    //             promotion.convertTo = (PieceType)pieceType;
-    //             promotion.promotion = true;
-    //             pseudoLegalMoves.push_back(promotion);
-    //         }
-    //     }
-    // }
-    // if (CheckBit(captures, 7, promotionRank))
-    // {
-    //     if (CheckBit(engine.board.pieceBoards[colorIndex], 7 - 1, promotionRank - colorDirection)) // Captures are diagonal moves
-    //     {
-    //         Move promotion = Move(7 - 1, promotionRank - colorDirection, 7, promotionRank);
-    //         for (auto pieceType = 2; pieceType < 6; pieceType++)
-    //         {
-    //             promotion.convertTo = (PieceType)pieceType;
-    //             promotion.promotion = true;
-    //             pseudoLegalMoves.push_back(promotion);
-    //         }
-    //     }
-    // }
+    uint_fast64_t pawnBoard = engine.board.pieceBoards[colorIndex];
+    auto from = BitScanForwards(pawnBoard)-1;
+    while (from>=0)
+    {
+        for(auto j=0;j<2;j++){
+            uint64_t captures = pawnAttacks[!color][j][from] & (engine.board.colorBoards[color]|engine.board.ghostBoard);
+            to = BitScanForwards(captures)-1;
+            if(to>=0)
+            {
+                Move move(from,to);
+                if(CheckBit(rankMasks[promotionRank],to)){
+                    for(auto i=2;i<6;i++){
+                        move.convertTo = (PieceType)i;
+                        move.promotion = true;
+                        pseudoLegalMoves.push_back(move);
+                    }
+                }
+                else{
+                    pseudoLegalMoves.push_back(move);
+                }
+            }
+        }
+        UnsetBit(pawnBoard,from);
+        from = BitScanForwards(pawnBoard)-1;
+    }
 }
 
 void GetPseudoLegalKnightMoves(const Engine &engine, std::vector<Move> &pseudoLegalMoves)
@@ -256,7 +136,6 @@ void GetPseudoLegalKingMoves(const Engine &engine, std::vector<Move> &pseudoLega
     int kingIndex = engine.board.kingIndices[!color];
     auto castleColorIndex = color ? 0 : 2;                                                    // to obtain colored castling rules
     uint_fast64_t combinedColors = engine.board.colorBoards[0] | engine.board.colorBoards[1]; // | engine.board.attackBoard; // Bitboard with both friendly and unfriendly blockers and attacked squares for castling
-    auto firstRank = color ? 7 : 0;
 
     // normal king moves
     uint_fast64_t attacks = kingMoves[kingIndex] & ~engine.board.colorBoards[!color];
