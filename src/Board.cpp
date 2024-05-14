ce848ce6b99a4c7b8c80bdff7669384cd01e9200
giorgio <giorgio.lovato99@gmail.com>
2024-05-15 00:52:06 +0200

    v0.11: optimize makemove and make simple move

--- src/Board.cpp
+++ src/Board.cpp
@@ -23,83 +23,106 @@ void Board::MakeMove(const move &move)
 
 	int startIndex = StartIndex(move);
 	int targetIndex = TargetIndex(move);
+	bitboard start = ONE << startIndex;
+	bitboard target = ONE << targetIndex;
 
-	auto wasCastled = false; // Castling flag
-
-	auto color = (flags&1)==1;
+	auto color = (flags & 1) == 1;
 	auto colorIndex = color ? 0 : 6;
 	auto otherColorIndex = color ? 6 : 0;
 	auto colorDirection = color ? -1 : 1;
 
-	// Pawn double pushes. leaves ghost
-	if (CheckBit(pieceBoards[colorIndex], startIndex))
+	// Find Piece at start and move it to target
+	int startPiece = 0;
+	for (int i = 0; i < 6; i++)
 	{
-		if (std::abs(startIndex - targetIndex) == 16)
+		if ((pieceBoards[i + colorIndex] & start) > 0)
 		{
-			SetBit(ghostBoard, startIndex + colorDirection * 8);
-		}
-	}
-	// Enpassant
-	if (CheckBit(pieceBoards[colorIndex], startIndex) && !CheckBit(colorBoards[color], targetIndex))
-	{
-		if (std::abs(startIndex - targetIndex) == 9 || std::abs(startIndex - targetIndex) == 7)
-		{
-			UnsetBit(pieceBoards[otherColorIndex], targetIndex - colorDirection * 8);
-		}
-	}
-
-	for (auto i = colorIndex; i < 6 + colorIndex; i++)
-	{ // go trough all pieces of the color to move and find the piece at start
-		// Generic moving
-		if (CheckBit(pieceBoards[i], startIndex))
-		{ // is piece found, set piece bitboard at target and unset at start
-			SetBit(pieceBoards[i], targetIndex);
-			UnsetBit(pieceBoards[i], startIndex);
-			wasCastled = (i == 5 + colorIndex) && startIndex == (color ? 60 : 4) && std::abs(startIndex - targetIndex) > 1; // flag if move was a castle
+			startPiece = i + colorIndex;
+			pieceBoards[startPiece] ^= start | target;
+			colorBoards[!color] ^= start|target;
 			break;
 		}
 	}
 
-	for (auto i = 0; i < 6; i++)
-	{ // for all not opposite color pieces, find piece at target and unset
-		UnsetBit(pieceBoards[i + otherColorIndex], targetIndex);
+	// Find Piece at target and remove it
+	int targetPiece = -1;
+	for (int i = 0; i < 6; i++)
+	{
+		if ((pieceBoards[i + otherColorIndex] & target) > 0)
+		{
+			targetPiece = i + otherColorIndex;
+			pieceBoards[targetPiece] ^= target;
+			colorBoards[color] ^=target;
+			break;
+		}
+	}
+
+	// Pawn double pushes. leaves ghost
+	if (startPiece == colorIndex)
+	{
+		auto diff = startIndex - targetIndex;
+		if (diff == 16)
+		{
+			ghostBoard |= start >> 8;
+		}
+		else if (diff == -16)
+		{
+			ghostBoard |= start << 8;
+		}
+	}
+
+	// Enpassant: if pawn moves to unoccupied square, remove enemy pawn at one rank below(above) target. Will only affect en passant moves
+	if (startPiece == colorIndex && targetPiece < 0)
+	{
+		// UnsetBit(pieceBoards[otherColorIndex], targetIndex - colorDirection * 8);
+		if (color)
+		{
+			pieceBoards[otherColorIndex] &= ~(target << 8);
+			colorBoards[color] &= ~(target << 8);
+		}
+		else
+		{
+			pieceBoards[otherColorIndex] &= ~(target >> 8);
+			colorBoards[color] &= ~(target >> 8);
+		}
 	}
 
 	// Castling (moving the rook)
-	if (wasCastled)
+	if (startPiece == 5 + colorIndex && startIndex == (color ? 60 : 4) && std::abs(startIndex - targetIndex) > 1)
 	{
 		auto castleTarget = color ? 62 : 6;
 		if (targetIndex == castleTarget)
 		{
-			SetBit(pieceBoards[3 + colorIndex], targetIndex - 1);
-			UnsetBit(pieceBoards[3 + colorIndex], targetIndex + 1);
+			pieceBoards[3 + colorIndex] ^= target >> 1 | target << 1;
+			colorBoards[!color] ^= target >> 1 | target << 1;
 		}
 		castleTarget = color ? 58 : 2;
 		if (targetIndex == castleTarget)
 		{
-			SetBit(pieceBoards[3 + colorIndex], targetIndex + 1);
-			UnsetBit(pieceBoards[3 + colorIndex], targetIndex - 2);
+			pieceBoards[3 + colorIndex] ^= target << 1 | target >> 2;
+			colorBoards[!color] ^= target << 1 | target >> 2;
 		}
 	}
 
 	// Promotions
-	if (Promotion(move)==1)
+	if (Promotion(move) == 1)
 	{
-		UnsetBit(pieceBoards[colorIndex], targetIndex);
-		SetBit(pieceBoards[colorIndex + ConvertTo(move)], targetIndex);
+		pieceBoards[colorIndex] ^= target;
+		colorBoards[!color] ^= target;
+		pieceBoards[colorIndex+ConvertTo(move)] ^= target;
+		colorBoards[!color] ^=target;
 	}
 
 	// Update castling flags
-	flags &= (startIndex == 63 || targetIndex == 63 || startIndex == 60)?0b00011101:31;
-	flags &= (startIndex == 56 || targetIndex == 56 || startIndex == 60)?0b00011011:31;
-	flags &= (startIndex == 7 || targetIndex == 7 || startIndex == 4)?0b00010111:31;
-	flags &= (startIndex == 0 || targetIndex == 0 || startIndex == 4)?0b00001111:31;
-
-	// Update Color Boards
-	UpdateColorBoards();
+	flags &= (startIndex == 63 || targetIndex == 63 || startIndex == 60) ? 0b00011101 : 31;
+	flags &= (startIndex == 56 || targetIndex == 56 || startIndex == 60) ? 0b00011011 : 31;
+	flags &= (startIndex == 7 || targetIndex == 7 || startIndex == 4) ? 0b00010111 : 31;
+	flags &= (startIndex == 0 || targetIndex == 0 || startIndex == 4) ? 0b00001111 : 31;
 
 	// Update King coords
-	UpdateKingIndices(startIndex,targetIndex);
+	kingIndices[!color] = (startPiece==(5+colorIndex))?targetIndex:kingIndices[!color];
+	kingIndices[color] = (startPiece==(5+otherColorIndex))?targetIndex:kingIndices[color];
+
 
 	// Update turn flag
 	flags ^= ONE;
@@ -107,47 +130,65 @@ void Board::MakeMove(const move &move)
 
 void Board::MakeSimpleMove(const move &move)
 {
-	// Clear ghosts
+		// Clear ghosts
 	ghostBoard = ZERO;
 
 	int startIndex = StartIndex(move);
 	int targetIndex = TargetIndex(move);
+	bitboard start = ONE << startIndex;
+	bitboard target = ONE << targetIndex;
 
-	auto color = (flags&1)==1;
+	auto color = (flags & 1) == 1;
 	auto colorIndex = color ? 0 : 6;
 	auto otherColorIndex = color ? 6 : 0;
 	auto colorDirection = color ? -1 : 1;
-	// Enpassant
-	if (CheckBit(pieceBoards[colorIndex], startIndex) && !CheckBit(colorBoards[color], targetIndex))
+
+	// Find Piece at start and move it to target
+	int startPiece = 0;
+	for (int i = 0; i < 6; i++)
 	{
-		if (std::abs(startIndex - targetIndex) == 9 || std::abs(startIndex - targetIndex) == 7)
+		if ((pieceBoards[i + colorIndex] & start) > 0)
 		{
-			UnsetBit(pieceBoards[otherColorIndex], targetIndex - colorDirection * 8);
-		}
-	}
-	for (auto i = colorIndex; i < 6 + colorIndex; i++)
-	{ // go trough all pieces of the color to move and find the piece at start
-		// Generic moving
-		if (CheckBit(pieceBoards[i], startIndex))
-		{ // is piece found, set piece bitboard at target and unset at start
-			SetBit(pieceBoards[i], targetIndex);
-			UnsetBit(pieceBoards[i], startIndex);
+			startPiece = i + colorIndex;
+			pieceBoards[startPiece] ^= start | target;
+			colorBoards[!color] ^= start|target;
 			break;
 		}
 	}
 
-	for (auto i = 0; i < 6; i++)
-	{ // for all not to move pieces, find piece at target and unset
-		UnsetBit(pieceBoards[i + otherColorIndex], targetIndex);
+	// Find Piece at target and remove it
+	int targetPiece = -1;
+	for (int i = 0; i < 6; i++)
+	{
+		if ((pieceBoards[i + otherColorIndex] & target) > 0)
+		{
+			targetPiece = i + otherColorIndex;
+			pieceBoards[targetPiece] ^= target;
+			colorBoards[color] ^=target;
+			break;
+		}
 	}
 
-	// TO DO: Maybe check for checks?
-
-	// Update Color Boards
-	UpdateColorBoards();
+	// Enpassant: if pawn moves to unoccupied square, remove enemy pawn at one rank below(above) target. Will only affect en passant moves
+	if (startPiece == colorIndex && targetPiece < 0)
+	{
+		// UnsetBit(pieceBoards[otherColorIndex], targetIndex - colorDirection * 8);
+		if (color)
+		{
+			pieceBoards[otherColorIndex] &= ~(target << 8);
+			colorBoards[color] &= ~(target << 8);
+		}
+		else
+		{
+			pieceBoards[otherColorIndex] &= ~(target >> 8);
+			colorBoards[color] &= ~(target >> 8);
+		}
+	}
 
 	// Update King coords
-	UpdateKingIndices(startIndex,targetIndex);
+	kingIndices[!color] = (startPiece==(5+colorIndex))?targetIndex:kingIndices[!color];
+	kingIndices[color] = (startPiece==(5+otherColorIndex))?targetIndex:kingIndices[color];
+
 
 	// Update turn flag
 	flags ^= ONE;
@@ -166,13 +207,13 @@ void Board::UpdateColorBoards()
 
 void Board::InitialiseKingIndices()
 {
-	kingIndices[0] = BitScanForwards(pieceBoards[5])-1;
-	kingIndices[1] = BitScanForwards(pieceBoards[11])-1;
+	kingIndices[0] = BitScanForwards(pieceBoards[5]) - 1;
+	kingIndices[1] = BitScanForwards(pieceBoards[11]) - 1;
 }
 
-void Board::UpdateKingIndices(const int& startIndex, const int&targetIndex)
+void Board::UpdateKingIndices(const int &startIndex, const int &targetIndex)
 {
-	kingIndices[!((flags&1)==1)] = kingIndices[!((flags&1)==1)] == startIndex ? targetIndex : kingIndices[!((flags&1)==1)];
+	kingIndices[!((flags & 1) == 1)] = kingIndices[!((flags & 1) == 1)] == startIndex ? targetIndex : kingIndices[!((flags & 1) == 1)];
 }
 
 std::string Board::ShowBoard()
