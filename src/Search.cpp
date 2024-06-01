97e80bda20795a3c9e21bb7debcca3c576da0078
giorgio <giorgio.lovato99@gmail.com>
2024-06-01 23:01:09 +0200

    clean up

--- src/Search.cpp
+++ src/Search.cpp
@@ -69,66 +69,6 @@ move Search::GetBestMove(Engine &engine, Timer &timer)
 
 // Internal Functions
 // --------------------------------------------------------
-int Min(Engine &engine, int depthRemaining)
-{
-    if (depthRemaining == 0) // If the depth limit is reached I evaluate the position. However, since it is my opponents turn to move and the evaluation is from his perspective, I flip the sign
-    {
-        return -Evaluation::StaticEvaluation(engine);
-    }
-
-    if (std::count(engine.repetitionTable.begin(), engine.repetitionTable.end(), engine.currentZobristKey) >= 2)
-    {
-        return 0;
-    }
-
-    std::array<move, 256> moveHolder;
-    uint moveHolderIndex = 0;
-    engine.GetLegalMoves(moveHolder, moveHolderIndex, false);
-
-    int bestScore = -INT32_MAX;
-    for (uint i = 0; i < moveHolderIndex; i++) // If the depth limit is not reached, I look for the highest score I can achieve from this position
-    {
-        engine.MakeMove(moveHolder[i]);
-        int tmpScore = Max(engine, depthRemaining - 1);
-        engine.UndoLastMove();
-        if (tmpScore >= bestScore)
-        {
-            bestScore = tmpScore;
-        }
-    }
-    return bestScore;
-}
-
-int Max(Engine &engine, int depthRemaining)
-{
-    if (depthRemaining == 0) // If the depth limit is reached I evaluate the position from my perspective
-    {
-        return Evaluation::StaticEvaluation(engine);
-    }
-
-    if (std::count(engine.repetitionTable.begin(), engine.repetitionTable.end(), engine.currentZobristKey) >= 2)
-    {
-        return 0;
-    }
-
-    std::array<move, 256> moveHolder;
-    uint moveHolderIndex = 0;
-    engine.GetLegalMoves(moveHolder, moveHolderIndex, false);
-
-    int bestScore = INT32_MAX;
-    for (uint i = 0; i < moveHolderIndex; i++) // If the depth limit is not reached, I look for the bestmove of my opponent, meaning the lowest score from my perspective in the given position
-    {
-        engine.MakeMove(moveHolder[i]);
-        int tmpScore = Min(engine, depthRemaining - 1);
-        engine.UndoLastMove();
-
-        if (tmpScore <= bestScore)
-        {
-            bestScore = tmpScore;
-        }
-    }
-    return bestScore;
-}
 
 int AlphaBetaMin(Engine &engine, int alpha, int beta, int depthRemaining)
 {
@@ -284,49 +224,4 @@ int QuiescenceMax(Engine &engine, int alpha, int beta)
     return beta;
 }
 
-int FixedDepthSearch(Engine &engine, int depth)
-{
-    if (depth == engine.maxDepth || engine.stopFlag) // If max depth is reached or search is stopped return static evaluation
-    {
-        return Evaluation::StaticEvaluation(engine);
-    }
-    std::array<move, 256> moveHolder;
-    uint moveHolderIndex = 0;
-    engine.GetLegalMoves(moveHolder, moveHolderIndex, false);
-    int score = -INT32_MAX;
-    for (uint i = 0; (i < moveHolderIndex) && (!engine.stopFlag); i++) // Loop over all legal moves and perform fixed depth-1 negamax search
-    {
-        engine.MakeMove(moveHolder[i]);
-        int tempScore = -FixedDepthSearch(engine, depth + 1);
-        engine.UndoLastMove();
-        score = tempScore > score ? tempScore : score; // Only keep highest score
-    }
-    return score;
-}
-
-int AlphaBetaSearch(Engine &engine, int alpha, int beta, int depth)
-{
-    if (depth == engine.maxDepth || engine.stopFlag)
-    {
-        return Evaluation::StaticEvaluation(engine);
-    }
-    std::array<move, 256> moveHolder;
-    uint moveHolderIndex = 0;
-    engine.GetLegalMoves(moveHolder, moveHolderIndex, false);
-    for (uint i = 0; (i < moveHolderIndex) && (!engine.stopFlag); i++)
-    {
-        engine.MakeMove(moveHolder[i]);
-        int tmpScore = -AlphaBetaSearch(engine, -beta, -alpha, depth + 1);
-        engine.UndoLastMove();
-        if (tmpScore >= beta)
-        {
-            return beta;
-        }
-        if (tmpScore > alpha)
-        {
-            alpha = tmpScore;
-        }
-    }
-    return alpha;
-}
 // --------------------------------------------------------
