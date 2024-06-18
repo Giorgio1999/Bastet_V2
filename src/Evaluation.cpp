6d2e5c42708bb832f09a92aaec210155bed0c61c
giorgio <giorgio.lovato99@gmail.com>
2024-06-18 18:30:42 +0200

    final commit on this branch

--- src/Evaluation.cpp
+++ src/Evaluation.cpp
@@ -11,8 +11,7 @@
 int Evaluation::StaticEvaluation(Engine &engine)
 {
     Board &currentBoard = engine.CurrentBoard();
-    // auto colorMultiplier = (currentBoard.flags & 1) == 1 ? -1 : 1; // If white is maximizing player, then after a white move it is blacks turn. So the score will be #white-#black. If black is maximizing then its whites turn and #black-#white
-
+    
     int evaluation = 0;
     // bitboard countBoard = ZERO;
     for (auto i = 0; i < 6; i++)
@@ -21,13 +20,13 @@ int Evaluation::StaticEvaluation(Engine &engine)
         // while (countBoard > 0)
         // {
         //     square square = PopLsb(countBoard);
-        //     evaluation += colorMultiplier * pieceSquareTables[0][i][square];
+        //     evaluation += pieceSquareTables[0][i][square];
         // }
         // countBoard = currentBoard.pieceBoards[i + 6];
         // while (countBoard > 0)
         // {
         //     square square = PopLsb(countBoard);
-        //     evaluation -= colorMultiplier * pieceSquareTables[1][i][square];
+        //     evaluation -= pieceSquareTables[1][i][square];
         // }
 
         evaluation += pieceValues[i] * (NumberOfSetBits(currentBoard.pieceBoards[i]) - NumberOfSetBits(currentBoard.pieceBoards[i + 6])); // Count pieces multiplied with piece Values
