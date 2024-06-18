d1bf77d7d82f4151b2dfdc8c361c0a70df32a630
giorgio <giorgio.lovato99@gmail.com>
2024-06-19 01:26:48 +0200

    pst and endgame (when the queens have left)

--- src/Evaluation.cpp
+++ src/Evaluation.cpp
@@ -11,28 +11,33 @@
 int Evaluation::StaticEvaluation(Engine &engine)
 {
     Board &currentBoard = engine.CurrentBoard();
-    
+
     int evaluation = 0;
-    // bitboard countBoard = ZERO;
+    bitboard countBoard = currentBoard.pieceBoards[5] | currentBoard.pieceBoards[10];
+    auto queensThere = countBoard > 0;
     for (auto i = 0; i < 6; i++)
     {
-        // countBoard = currentBoard.pieceBoards[i];
-        // while (countBoard > 0)
-        // {
-        //     square square = PopLsb(countBoard);
-        //     evaluation += pieceSquareTables[0][i][square];
-        // }
-        // countBoard = currentBoard.pieceBoards[i + 6];
-        // while (countBoard > 0)
-        // {
-        //     square square = PopLsb(countBoard);
-        //     evaluation -= pieceSquareTables[1][i][square];
-        // }
+        if (queensThere)
+        {
+            countBoard = currentBoard.pieceBoards[i];
+            while (countBoard > 0)
+            {
+                square square = PopLsb(countBoard);
+                evaluation += pieceSquareTables[0][i][square];
+            }
+
+            countBoard = currentBoard.pieceBoards[i + 6];
+            while (countBoard > 0)
+            {
+                square square = PopLsb(countBoard);
+                evaluation -= pieceSquareTables[1][i][square];
+            }
+        }
 
         evaluation += pieceValues[i] * (NumberOfSetBits(currentBoard.pieceBoards[i]) - NumberOfSetBits(currentBoard.pieceBoards[i + 6])); // Count pieces multiplied with piece Values
     }
     // evaluation += whiteBonus; // If white is maximizing (black to play in the current position) then add tempo bonus to the evaluation. If black is maximizing subtract tempo
-    evaluation += engine.prng -> Next(-20, 20); // Always add a random jiggle to the evaluation, which emulates mobility scores
+    evaluation += engine.prng->Next(-20, 20); // Always add a random jiggle to the evaluation, which emulates mobility scores
     return evaluation;
 }
 // -------------------------------------------------------------------
