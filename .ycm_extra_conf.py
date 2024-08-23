c9c7bb68229fbe8c1c5f6f7140c115bde5295fcd
Giorgio Lovato <giorgio.lovato99@gmail.com>
2024-08-24 00:57:40 +0200

    ycm_extra_conf

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,6 +1,13 @@
 import os
 import ycm_core
+import subprocess
 
+fin_includepath = subprocess.run(["echo | g++ -xc -xc++ -E -v - 2>&1 | sed -n '/#include <...> search starts here:/,/End of search list./p'"],shell=True,capture_output=True,text=True)
+includepaths = fin_includepath.stdout.split('\n')
+includeflags = []
+for i in range(1,len(includepaths)-1):
+    includeflags.append('-isystem')
+    includeflags.append(includepaths[i].split(' ')[1])
 # Diese Flags sind für g++ Konfigurationen
 flags = [
         '-Wall',
@@ -8,12 +15,13 @@ flags = [
         '-Werror',
         '-std=c++20',
         '-x','c++',
-        '-I','/home/giorgio/Bastet_V2/header',
-        '-I','/usr/include',
-        '-I','/usr/local/include',
+        '-I','header'
         ]
+flags.extend(includeflags)
+
+
 # Diese FUntion findet den Compile-Flags für die aktuelle Conf
 def Settings(**kwards):
     return {
             'flags':flags
-           }
+            } 
